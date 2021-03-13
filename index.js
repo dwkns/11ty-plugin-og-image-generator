e1bdd7910cdfb263edbe0bb46500cae798f6b6dd
dwkns <dwkns@me.com>
2021-03-13 15:38:24 +0000

    Update linting. Add real functionilty

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log(' Hello ');
+const createOGImage = (text = 'default text') => {
+  const output = createImage(text); 
+  return output;
+};
+
+function createImage(text) {
+  return `Image will be created with: ${ text }`;
+}
+
+exports.createOGImage = createOGImage;
